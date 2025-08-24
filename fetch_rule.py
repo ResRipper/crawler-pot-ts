32ee5bb6b1a3dcae5b167981b4eda17bf9c8a317
ResRipper <resripper@connective.link>
2025-08-24 17:04:00 +0800

    Code cleanup

--- fetch_rule.py
+++ fetch_rule.py
@@ -1,31 +1,40 @@
 #!/bin/python3
 import json
-from urllib.request import urlopen, Request
+import sys
 from os import environ
+from urllib.request import Request
+from urllib.request import urlopen
 
-if __name__ == "__main__":
-    zone_id = environ.get("ZONE_ID")
-    ruleset_id = environ.get("RULESET_ID")
-    api_token = environ.get("API_TOKEN")
-    rule_id = environ.get("RULE_ID")
+if __name__ == '__main__':
+    zone_id = environ.get('ZONE_ID')
+    ruleset_id = environ.get('RULESET_ID')
+    api_token = environ.get('API_TOKEN')
+    rule_id = environ.get('RULE_ID')
 
-    if zone_id and ruleset_id and api_token and rule_id is None:
-        exit('ENV not found')
+    if (zone_id and ruleset_id and api_token and rule_id) is None:
+        raise ValueError('ENV not found')
 
     with urlopen(
         Request(
-            url=f"https://api.cloudflare.com/client/v4/zones/{zone_id}/rulesets/{ruleset_id}",
-            method="GET",
-            headers={"Authorization": f"Bearer {api_token}"},
+            url=f'https://api.cloudflare.com/client/v4/zones/{zone_id}/rulesets/{ruleset_id}',
+            method='GET',
+            headers={'Authorization': f'Bearer {api_token}'},
         )
     ) as req:
-        ruleset = json.loads(req.read().decode("utf-8"))
+        if req.status != 200:
+            raise ValueError(f'{req.status}: {req.read().decode("utf-8")}')
+        else:
+            ruleset: dict = json.loads(req.read().decode('utf-8'))
 
-        for item in ruleset["result"]["rules"]:
-            if item["id"] == rule_id:
-                rule = item["expression"]
-                print(f'Current rule: {rule}')
+            for item in ruleset['result']['rules']:
+                if item['id'] == rule_id:
+                    rule = item['expression']
+                    print(f'Current rule: {rule}')
 
-                with open("./current_rule.txt", "w") as f:
-                    f.truncate()
-                    f.write(rule)
+                    # Overwrite currrent file
+                    with open('./current_rule.txt', 'w') as f:
+                        f.truncate()
+                        f.write(rule)
+                        sys.exit()
+
+            raise ValueError(f'Rule {rule_id} not found')
